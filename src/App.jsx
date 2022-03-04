4358a415b436ed3d5471e62968fe70b08134ae26
Edvard <eduardosantamarco123@outlook.com>
2022-03-04 17:05:25 -0300

    mobile first

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import {React, Component, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App()
+class App extends Component
 {
-  return( 
+  constructor(props)
+  {
+   super(props);
+   this.state =
+   { 
+      name: ""
+   }
+  }
+  render()
+  { return(
     <div>
-     <form>
-
-     </form>
+	<form classname="in">
+	  <h2>Contato</h2>
+         <input type="text"id="name" name="username" value={this.state.name} onChange={(e) => this.setState({name: e.target.value})} placeholder="Digite o seu nome"/>
+	</form>
     </div>
-  )
+  )}
 }
 export default App
